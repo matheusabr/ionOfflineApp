ef5620ba866bb379ec70cd17ec41b6cf20e06fde
Matt Abreu <mattabreu@Matts-Air.lan>
2019-12-16 00:00:57 +0000

    task(HomePage): show a updated list of users got by api requests

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,12 +1,31 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { ApiService } from '../services/api.service';
+import { Platform } from '@ionic/angular';
 
 @Component({
   selector: 'app-home',
   templateUrl: 'home.page.html',
-  styleUrls: ['home.page.scss'],
+  styleUrls: ['home.page.scss']
 })
-export class HomePage {
+export class HomePage implements OnInit {
+  users = [];
 
-  constructor() {}
+  constructor(private apiService: ApiService, private plt: Platform) {}
 
+  ngOnInit() {
+    this.plt.ready().then(() => this.loadData(true));
+  }
+
+  private loadData(refresh = false, refresher?) {
+    this.apiService.getUsers(refresh).subscribe(res => {
+      this.users = res;
+      if (refresher) {
+        refresher.target.complete();
+      }
+    });
+  }
+
+  private updateUser(id) {
+    this.apiService.updateUser(id, { name: 'Matt', job: 'WebDev' }).subscribe();
+  }
 }
